eccfc145f33e843efa7f39190622aa94c7cb2a52
Nikita <guschinnick@gmail.com>
2020-12-07 22:49:34 +0300

    dataModel when

--- app/src/main/java/com/qoiu/translator/MainActivity.kt
+++ app/src/main/java/com/qoiu/translator/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.qoiu.translator
 
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.textfield.TextInputEditText
 import com.qoiu.translator.mvp.view.BaseActivity
@@ -35,21 +38,40 @@ class MainActivity : BaseActivity<DataModel>() {
             is DataModel.Success->{
                 val searchResult = dataModel.data
                 if(searchResult.isEmpty()){
-
+                   hideView()
                 } else {
+                    showView(main_layout_results)
                     if (adapter == null) {
                         recycler_view.layoutManager= LinearLayoutManager(baseContext)
                         recycler_view.adapter=MainViewRecycler(searchResult)
                     } else {
-                        showViewSuccess()
                         adapter.setData(searchResult)
                     }
                 }
             }
+            is DataModel.Loading -> {
+                showView(main_layout_progress)
+                if(dataModel.progress != null){
+                    progressBar.progress=dataModel.progress
+                } else{
+                   hideView()
+                }
+            }
+            is DataModel.Error->{
+                showView(main_layout_error)
+                textview_error.text=dataModel.error.message
+            }
         }
     }
 
-    private fun showViewSuccess(){
+    private fun showView(view: LinearLayout){
+        hideView()
+        view.visibility= VISIBLE
+    }
 
+    private fun hideView(){
+        main_layout_error.visibility=GONE
+        main_layout_progress.visibility=GONE
+        main_layout_results.visibility=GONE
     }
 }
